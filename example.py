daca92051916b522d0e884b5b7fbc74b7a648755
Kenneth Carrell <kennethcarrell@gmail.com>
2021-02-01 16:03:36 -0600

    Added doc

--- example.py
+++ example.py
@@ -1,10 +1,6 @@
-# ATARRI
-A TESS Archive RR Lyrae Classifier
-
-###### Example usage:
-```python
+import tkinter
 from ATARRI.RRLClassifier import RRLClassifier
+
 root = tkinter.Tk()
 theGui = RRLClassifier(root)
 root.mainloop()
-```
